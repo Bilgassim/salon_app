6b732164755db566b645a31aad2ea577074f399f
Health_App <moussa.moubarak.23@ump.ac.ma>
2026-07-29 02:33:04 +0200

    fix: remove hardcoded blocked slots

--- src/app/pages/Reservation.tsx
+++ src/app/pages/Reservation.tsx
@@ -49,14 +49,15 @@ const SERVICES = [
 ];
 
 const TIMESLOTS = [
-  { time: "10:30", available: false }, { time: "11:00", available: true },
-  { time: "11:30", available: true },  { time: "12:00", available: false },
-  { time: "12:30", available: true },  { time: "13:00", available: true },
-  { time: "14:00", available: false }, { time: "14:30", available: true },
-  { time: "15:00", available: true },  { time: "15:30", available: false },
-  { time: "16:00", available: true },  { time: "17:00", available: true },
-  { time: "17:30", available: false }, { time: "18:00", available: true },
-  { time: "18:30", available: true },  { time: "19:00", available: true },
+  { time: "10:30", available: true }, { time: "11:00", available: true },
+  { time: "11:30", available: true }, { time: "12:00", available: true },
+  { time: "12:30", available: true }, { time: "13:00", available: true },
+  { time: "13:30", available: true }, { time: "14:00", available: true },
+  { time: "14:30", available: true }, { time: "15:00", available: true },
+  { time: "15:30", available: true }, { time: "16:00", available: true },
+  { time: "16:30", available: true }, { time: "17:00", available: true },
+  { time: "17:30", available: true }, { time: "18:00", available: true },
+  { time: "18:30", available: true }, { time: "19:00", available: true },
   { time: "19:30", available: true },
 ];
 
